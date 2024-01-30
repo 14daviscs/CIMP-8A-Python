8ca2a25e382b84dc07056646de8f077b3108247f
14daviscs <145870942+14daviscs@users.noreply.github.com>
2024-01-29 19:40:15 -0800

    progress

--- guess_number.py
+++ guess_number.py
@@ -2,22 +2,27 @@
 
 import random
 
-randomNumber = random.randint(1,9)
+# Get a random number between 1 and 9
+randomNumber = random.randint(1, 9)
 yourGuess = 0
 count = 0
 
+# Continue guessing until the guess is correct or the user types 'exit'
 while yourGuess != randomNumber and yourGuess != "exit":
     yourGuess = input("Enter a guess between 1 to 9 or 'exit' to end the game: ")
 
+    # If the user types 'exit', end the application
     if yourGuess == "exit":
         break
 
+    # Cast the user's guess to an integer so that it
+    # can be used in the comparison below
     yourGuess = int(yourGuess)
     count += 1
 
     if yourGuess < randomNumber:
         print("Too low")
-        elif yourGuess > randomNumber:
+    elif yourGuess > randomNumber:
         print("Too high")
     else:
         print("Right!")
