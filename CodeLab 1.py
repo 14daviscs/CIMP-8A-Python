8fb26f914ba5e790feeda723fcca59d22091aad9
14daviscs <145870942+14daviscs@users.noreply.github.com>
2024-01-29 18:51:41 -0800

    starting

---
+++ CodeLab 1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+print("BLACK JACK")
+print("Blackjack payout is 3:2")
+print()
+print("Starting money:   100")
+print("Bet amount:       5")
+print()
+print("ENDING MONEY FOR A...")
+print("Blackjack:        107.5")
+print("Win:              105.0")
+print("Push:             100.0")
+print("Lose:             95.0")
+print()
+print("Bye!")
+
+player_start = 100
+player_bet = input(int() or float())
+
+
+
