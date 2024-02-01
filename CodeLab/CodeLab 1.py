ff29e636a746ec2571073938a1fe4d999542657e
14daviscs <145870942+14daviscs@users.noreply.github.com>
2024-01-31 19:50:45 -0800

    Update CodeLab 1.py

--- CodeLab/CodeLab 1.py
+++ CodeLab/CodeLab 1.py
@@ -1,21 +1,21 @@
 #! /usr/bin/env python3
 
-print("BLACKJACK!")
+print("BLACK JACK")
 print("Blackjack payout is 3:2")
 print()
 
-player_start = float(input("Starting Money:    "))
-player_bet = float(input("Bet Amount:          "))
+player_start = float(input("Starting money:   "))
+player_bet = float(input("Bet amount:       "))
 
 blackjack = round(player_start + (player_bet * 1.5), 2)
 win = round(player_start + player_bet, 2)
-fold = round(player_start - player_bet, 2)
+lose = round(player_start - player_bet, 2)
 
 print()
 print("ENDING MONEY FOR A...")
-print("Blackjack:        ", blackjack)
-print("Win:              ", win)
-print("Push:             ", player_start)
-print("Lose:             ", fold)
+print("Blackjack:       ", blackjack)
+print("Win:             ", win)
+print("Push:            ", player_start)
+print("Lose:            ", lose)
 print()
-print("See you next time!")
+print("Bye!")
