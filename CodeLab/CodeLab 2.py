7455245c33bc10414ca74a6ff5b6c7ba4f791291
14daviscs <145870942+14daviscs@users.noreply.github.com>
2024-02-11 16:03:30 -0800

    actually finish codelab 2

--- CodeLab/CodeLab 2.py
+++ CodeLab/CodeLab 2.py
@@ -3,28 +3,28 @@
 # Greet player
 print("BLACKJACK!")
 print("Blackjack payout is 3:2")
-print()
+print("Enter 'x' for bet to exit\n")
 
 # Initialize variables
 player_money = float(input("Starting money:   "))
 print()
-player_bet = float(input("Bet amount: "))
+player_bet = input("Bet amount: ")
 result = ''
 
 # Loop until user exits
-while result.lower != 'x':
+while result != 'x':
     result = input("Blackjack, win, push, or lose (b/w/p/l): ")
     if result == 'b':
-        player_money = round(player_money + (player_bet * 1.5), 2)
+        player_money = round(player_money + float(player_bet) * 1.5, 2)
         print(f"Money: {player_money}\n\nBet amount: {player_bet}")
     elif result == 'w':
-        player_money = round(player_money + player_bet, 2)
+        player_money = round(player_money + float(player_bet), 2)
         print(f"Money: {player_money}\n\nBet amount: {player_bet}")
     elif result == 'p':
         print(f"Money: {player_money}\n\nBet amount: {player_bet}")
     elif result == 'l':
-        player_money = round(player_money - player_bet, 2)
+        player_money = round(player_money - float(player_bet), 2)
         print(f"Money: {player_money}\n\nBet amount: {player_bet}")
 
 # End program
-print("Goodbye")
+print("\nGoodbye!")
