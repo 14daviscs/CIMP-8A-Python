86a11e32bbe795a5676f835e20a36b3cb03b6820
14daviscs <145870942+14daviscs@users.noreply.github.com>
2024-03-09 17:52:12 -0800

    extra credit?

---
+++ assignment 6/quarterly_sales.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def storage():
+    quarterly_sales = []
+
+def start_inputs():
+    print("The Quarterly Sales Program")
+    print()
+    quarterly_sales[0] = float(input("Enter sales for Q1: "))
+    quarterly_sales[1] = float(input("Enter sales for Q2: "))
+    quarterly_sales[2] = float(input("Enter sales for Q3: "))
+    quarterly_sales[3] = float(input("Enter sales for Q4: "))
+    print()
+    print(f"Total:                 {quarterly_sales[0] + quarterly_sales[1] + quarterly_sales[2] + quarterly_sales[3]}")
+    print(f"Average Quarter:       {}")
+    print(f"Lowest Quarter:        {}")
+    print(f"Highest Quarter        {}")
+
+
+def main():
+    storage()
+    start_inputs()
+
+
+if __name__ == "__main__":
+    main()
